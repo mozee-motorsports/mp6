643de1800e9e3f274a0e25a3cdf25660ee481f2a
gardipeez <gardipeez@msoe.edu>
2023-11-28 17:55:00 -0600

    Zach's added throttle by wire

--- Core/Src/throttle.c
+++ Core/Src/throttle.c
@@ -29,9 +29,37 @@ int servo_init( void ) {
     servo_init_a.Pull = GPIO_NOPULL;
     servo_init_a.Speed = GPIO_SPEED_FREQ_HIGH;
     HAL_GPIO_Init(GPIOA, &servo_init_a);
+
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
 }
 
 void task_poll_servo( void ) {
     servo_init();
 
+}
+
+/**
+ * gets the average of the two pots in Volts
+*/
+double pot_avg(){
+    double volt_avg;
+    __UINT32_t pot0 = GPIO_PIN_0; //I know wrong just a placeholder
+    __UINT32_t pot1 = GPIO_PIN_1; //I know wrong just a placeholder
+
+    // Get ADC value
+    HAL_ADC_Start(&pot0);
+    HAL_ADC_PollForConversion(&pot0, HAL_MAX_DELAY);
+    int value0 = HAL_ADC_GetValue(&pot0);
+
+    HAL_ADC_Start(&pot1);
+    HAL_ADC_PollForConversion(&pot1, HAL_MAX_DELAY);
+    int value1 = HAL_ADC_GetValue(&pot1);
+
+    //converts reading into volts
+    pot0 = (3.3 / 4095) * pot0;
+    pot1 = (3.3 / 4095) * pot1;
+
+    volt_avg = (pot0 + pot1)/2;
+
+    return volt_avg;
 }
